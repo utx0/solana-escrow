79f6e871c3e75fdadf440dbb5e03fe168493159b
utx0_ <utx0@mettadata.com>
2021-12-24 01:34:04 +1100

    step 2

---
+++ src/error.rs
@@ -0,0 +1,14 @@
+use solana_program::program_error::ProgramError;
+use thiserror::Error;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
